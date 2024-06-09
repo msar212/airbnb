8af0a418dfa33191b1d06f746a8cbe2a0d607f39
我再吃一碗 <2650714479@qq.com>
2024-06-09 20:48:30 +0800

    搜索状态的切换动画效果

--- src/components/app-header/c-cpns/header-center/index.jsx
+++ src/components/app-header/c-cpns/header-center/index.jsx
@@ -1,4 +1,5 @@
 import React, { memo, useState } from 'react'
+import { CSSTransition } from 'react-transition-group'
 
 import IconSearchBar from '@/assets/svg/icon-search-bar'
 
@@ -6,36 +7,47 @@ import { CenterWrapper } from './style'
 import SearchTabs from './c-cpns/search-tabs'
 import SearchSections from './c-cpns/search-sections'
 
-import SearchTitles from "@/assets/data/search_titles"
+import SearchTitles from '@/assets/data/search_titles'
 
 
-const HeaderCenter = memo((props) => {
+const HeaderCenter = memo(props => {
   const { isSearch, searchBarClick } = props
 
   const [tabIndex, setTabIndex] = useState(0)
   const titles = SearchTitles.map(item => item.title)
 
-function searchBarClickHandle() {
-  if (searchBarClick) searchBarClick()
-}
+  function searchBarClickHandle () {
+    if (searchBarClick) searchBarClick()
+  }
 
   return (
     <CenterWrapper>
-      {!isSearch ? (
+      <CSSTransition
+        in={!isSearch}
+        classNames='bar'
+        timeout={250}
+        unmountOnExit={true}
+      >
         <div className='search-bar' onClick={searchBarClickHandle}>
-        <div className='text'>搜索房源和体验</div>
-        <div className='icon'>
-          <IconSearchBar />
+          <div className='text'>搜索房源和体验</div>
+          <div className='icon'>
+            <IconSearchBar />
+          </div>
         </div>
-      </div>
-      ) : (
+      </CSSTransition>
+      <CSSTransition
+        in={isSearch}
+        classNames='detail'
+        timeout={250}
+        unmountOnExit={true}
+      >
         <div className='search-detail'>
-        <SearchTabs titles={titles} />
-        <div className='infos'>
-          <SearchSections searchInfos={SearchTitles[tabIndex].searchInfos} />
+          <SearchTabs titles={titles} tabClick={setTabIndex} />
+          <div className='infos'>
+            <SearchSections searchInfos={SearchTitles[tabIndex].searchInfos} />
+          </div>
         </div>
-      </div>
-      )}
+      </CSSTransition>
     </CenterWrapper>
   )
 })
